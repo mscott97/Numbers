5ae8f9f51cf2145baaa82eafb143662b9f70f057
mscott97 <42581784+mscott97@users.noreply.github.com>
2019-09-20 00:59:44 -0500

    Initial commit

---
+++ Numbers.kts
@@ -0,0 +1,30 @@
+fun main(array: IntArray)
+{
+   var range: Byte = 112
+    println("$range")
+
+    var temp: short = -1224
+    println("$temp")
+
+    var score: Int = 100000
+    println("$Score")
+
+    var highScore: Long = 9999
+    println("$highScore")
+
+    var distance = 999.9
+    println("$distance")
+
+    var distance = 19.5F
+    println("$distance")
+
+    var letter: char
+    letter = 'k'
+    println("$letter")
+
+    var flag = true
+    println("$flag")
+
+
+
+}
